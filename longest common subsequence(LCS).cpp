3c4922ac3372fb0a3286b128d2d737b92d360f79
Gururaj-hegade <111405066+Gururaj-hegade@users.noreply.github.com>
2023-02-10 23:47:38 +0530

    Add files via upload

---
+++ longest common subsequence(LCS).cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int LCS(string text1,string text2){
+    int m = text1.size();
+    int n = text2.size();
+    vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+    for (int i = 1; i < m + 1; i++)
+    {
+        for (int j = 1; j < n + 1; j++)
+        {
+            if (text1[i - 1] == text2[j - 1])
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            else
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        }
+    }
+    return dp[m][n];
+}
+int main()
+{
+    string text1, text2;
+    cin >> text1;
+    cin >> text2;
+    int ans=LCS(text1,text2);
+    return 0;
+}
